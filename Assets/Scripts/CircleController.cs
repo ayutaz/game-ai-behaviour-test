aa028bf66737bf50a72f922b004d685cc702b260
yousan <ka1357amnbpdr@gmail.com>
2023-06-10 14:58:58 +0900

    feat: debug log

---
+++ Assets/Scripts/CircleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GameAiBehaviour;
+using UnityEngine;
+
+public class CircleController : MonoBehaviour
+{
+    private BehaviourTreeController _controller;
+    [SerializeField] private BehaviourTree behaviourTree;
+
+    private void Awake()
+    
+    {
+        _controller = new BehaviourTreeController();
+        _controller.TickInterval = 1.0f;
+        _controller.Setup(behaviourTree);
+    }
+
+    private void Start()
+    {
+        _controller.Update(Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        // _controller.Update(Time.deltaTime);
+    }
+}
